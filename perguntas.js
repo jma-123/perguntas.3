fa48ee7b61cb57a325dca64bf48d88ed1bf1018e
jma-123 <00001099070259sp@al.educacao.sp.gov.br>
2024-11-19 11:55:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'química',
+    ' Atualmente, quantos elementos químicos a tabela periódica possui?',
+    'atualmente são 118 elementos'
+)
+
+criaCartao(
+    'Cinema',
+    'Pantera Negra, filme da marvel, se passa em qual país fictício?',
+    'Wakanda'
+)
+
+criaCartao(
+    'Matemática',
+    ' Qual a mediana dos números 5,7,3,10,8,2?',
+    ' A mediana é 6'
+)
+
+criaCartao(
+    'Arte',
+    'As pirâmides de Quéops, Quéfren e Miquerinos, localizadas em Gizé, as estátuas de Akhenaton e o Templo de Luxor são exemplos de arquitetura e arte relacionados a qual civilização antiga?',
+    ' Egípcios'
+)
